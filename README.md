d1a8f63416552aaea6cc92b00d5d22127465f59f
Ram <vaishnav.rd@gmail.com>
2018-05-12 20:46:34 +0530

    added support for verbose mode

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# adblocker-for-youtube
+# Cross browser add-on adfree-youtube
 A cross browser add-on to remove all annoying Ads and Banners from YouTube.
+
+# Firefox listing link
+https://addons.mozilla.org/en-US/firefox/addon/ad-free-youtube/
+
+# Description
+* This add-on automatically hides all annoying ad banners from YouTube page.
+* This add-on also skips ad videos which you see before the video you want to watch.
+* Enjoy seamless experience from this very light weight add-on very.
+* This addon is completely safe to use as this addon removes ads from banner by double confirming that the add-on is not hiding any useful content accidentally.
+* This is an open source project, find the code on https://github.com/gurumukhi/YouTubeAdBlocker.
+
+Special Feature
+* This add-on supports verbose/debug mode on option page. This mode enables verbose logging in the browser console.
+
+Support:
+* Kindly feel free to open an issue on https://github.com/gurumukhi/YouTubeAdBlocker/issues for any support.
--- content_script.js
+++ content_script.js
@@ -1,12 +1,71 @@
-blockAd();
-setInterval(blockAd, 5000);
+// Wrapper method to execute when debug mode is on
+blockAdWithDebugging = () => {
+  console.log('AdFreeYouTube checking for advertisements on the page.');
+  ads = findAdDiv();
+  videoSkipAdButton = document.querySelector('div.videoAdUiSkipContainer');
+  if (!ads.length && !videoSkipAdButton) {
+        return;
+  }
 
-blockAd = () => {
-  document.querySelector('.ad-container').style.border= '2px solid red';
-  document.querySelector('#google_companion_ad_div').style.border= '2px solid red';
+  console.log('AdFreeYouTube found one or more advertisements on the page, highlighting them and removing from page after 2 seconds. To hide the ad immediately, disable verbose mode in addon options.');
+  ads.forEach((ad) => {
+    console.log(ad);
+    ad.style.border= '2px dotted red';
+  });
+  if (videoSkipAdButton) {
+    console.log(videoSkipAdButton.children[0]);
+    videoSkipAdButton.children[0].style.border = '2px solid red';
+  }
 
   setTimeout(() => {
-    document.querySelector('.ad-container').style.display = 'none';
-    document.querySelector('#google_companion_ad_div').style.display = 'none';
-  }, 1500);
+    blockAd();
+  }, 5000);
 };
+
+// Main method to remove ads
+blockAd = () => {
+  ads = findAdDiv();
+  videoSkipAdButton = document.querySelector('div.videoAdUiSkipContainer');
+  if (!ads.length && !videoSkipAdButton) {
+        return;
+  }
+
+  ads.forEach( (ad) => {
+    ad.style.display = 'none';
+  });
+  if (videoSkipAdButton) {
+    videoSkipAdButton.children[0].click();
+  }
+};
+
+// Method to find all ads on page
+findAdDiv = () => {
+  adDivArray = [];
+  videoFrameFooterAd = document.querySelector('.ad-container');
+  sidebarAd = document.querySelector('ytd-iframe-companion-renderer');
+  homepageAd = document.querySelector('#ad-iframe');
+
+  if (videoFrameFooterAd && videoFrameFooterAd.style.display !== 'none') {
+    adDivArray.push(videoFrameFooterAd);
+  }
+  if (sidebarAd && sidebarAd.style.display !== 'none') {
+    adDivArray.push(sidebarAd);
+  }
+  if (homepageAd && homepageAd.style.display !== 'none') {
+    adDivArray.push(homepageAd);
+  }
+  return adDivArray;
+};
+
+// Initialization
+let storageItem = browser.storage.local.get();
+storageItem.then((result) => {
+  if (result.isDebugModeOn) {
+    console.log('Hi from AdFreeYouTube, we are here to give you ad free experience on YouTube.');
+    blockAdWithDebugging();
+    setInterval(blockAdWithDebugging, 5000);
+  } else {
+    blockAd();
+    setInterval(blockAd, 5000);
+  }
+});
