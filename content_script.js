9b3efdaee65e70cb57b9c414889bd82aab1a7c74
Ram <vaishnav.rd@gmail.com>
2020-10-10 19:35:26 +0530

    1.3.0 - adds new ad div identifiers

--- content_script.js
+++ content_script.js
@@ -1,20 +1,29 @@
 // Wrapper method to execute when debug mode is on
 blockAdWithDebugging = () => {
-  console.log('AdFreeYouTube checking for advertisements on the page.');
+  console.log("AdFreeYouTube checking for advertisements on the page.");
   ads = findAdDiv();
-  videoSkipAdButton = document.querySelector('div.videoAdUiSkipContainer');
-  if (!ads.length && !videoSkipAdButton) {
-        return;
+  videoSkipAdButton1 = document.querySelector("div.videoAdUiSkipContainer");
+  videoSkipAdButton2 = document.querySelector(
+    "span.ytp-ad-skip-button-container"
+  );
+  if (!ads.length && !videoSkipAdButton1 && !videoSkipAdButton2) {
+    return;
   }
 
-  console.log('AdFreeYouTube found one or more advertisements on the page, highlighting them and removing from page after 2 seconds. To hide the ad immediately, disable verbose mode in addon options.');
+  console.log(
+    "AdFreeYouTube found one or more advertisements on the page, highlighting them and removing from page after 2 seconds. To hide the ad immediately, disable verbose mode in addon options."
+  );
   ads.forEach((ad) => {
     console.log(ad);
-    ad.style.border= '2px dotted red';
+    ad.style.border = "2px dotted red";
   });
-  if (videoSkipAdButton) {
-    console.log(videoSkipAdButton.children[0]);
-    videoSkipAdButton.children[0].style.border = '2px solid red';
+  if (videoSkipAdButton1) {
+    console.log(videoSkipAdButton1.children[0]);
+    videoSkipAdButton1.children[0].style.border = "2px solid red";
+  }
+  if (videoSkipAdButton2) {
+    console.log(videoSkipAdButton2.children[0]);
+    videoSkipAdButton2.children[0].style.border = "2px solid red";
   }
 
   setTimeout(() => {
@@ -25,37 +34,43 @@ blockAdWithDebugging = () => {
 // Main method to remove ads
 blockAd = () => {
   ads = findAdDiv();
-  videoSkipAdButton = document.querySelector('div.videoAdUiSkipContainer');
-  if (!ads.length && !videoSkipAdButton) {
-        return;
+  videoSkipAdButton1 = document.querySelector("div.videoAdUiSkipContainer");
+  videoSkipAdButton2 = document.querySelector(
+    "span.ytp-ad-skip-button-container"
+  );
+  if (!ads.length && !videoSkipAdButton1 && !videoSkipAdButton2) {
+    return;
   }
 
-  ads.forEach( (ad) => {
-    ad.style.display = 'none';
+  ads.forEach((ad) => {
+    ad.style.display = "none";
   });
-  if (videoSkipAdButton) {
-    videoSkipAdButton.children[0].click();
+  if (videoSkipAdButton1) {
+    videoSkipAdButton1.children[0].click();
+  }
+  if (videoSkipAdButton2) {
+    videoSkipAdButton2.children[0].click();
   }
 };
 
 // Method to find all ads on page
 findAdDiv = () => {
   adDivArray = [];
-  videoFrameFooterAd = document.querySelector('.ad-container');
-  sidebarVideoAd = document.querySelector('ytd-iframe-companion-renderer');
-  siderbarAdBanner = document.querySelector('ytd-companion-slot-renderer');
-  homepageAd = document.querySelector('#ad-iframe');
+  videoFrameFooterAd = document.querySelector(".ad-container");
+  sidebarVideoAd = document.querySelector("ytd-iframe-companion-renderer");
+  siderbarAdBanner = document.querySelector("ytd-companion-slot-renderer");
+  homepageAd = document.querySelector("#ad-iframe");
 
-  if (videoFrameFooterAd && videoFrameFooterAd.style.display !== 'none') {
+  if (videoFrameFooterAd && videoFrameFooterAd.style.display !== "none") {
     adDivArray.push(videoFrameFooterAd);
   }
-  if (sidebarVideoAd && sidebarVideoAd.style.display !== 'none') {
+  if (sidebarVideoAd && sidebarVideoAd.style.display !== "none") {
     adDivArray.push(sidebarVideoAd);
   }
-  if (siderbarAdBanner && siderbarAdBanner.style.display !== 'none') {
+  if (siderbarAdBanner && siderbarAdBanner.style.display !== "none") {
     adDivArray.push(siderbarAdBanner);
   }
-  if (homepageAd && homepageAd.style.display !== 'none') {
+  if (homepageAd && homepageAd.style.display !== "none") {
     adDivArray.push(homepageAd);
   }
   return adDivArray;
@@ -66,17 +81,26 @@ let storageItem = browser.storage.local.get();
 storageItem.then((result) => {
   // Setting ANNOTATIONS off
   let disabledAnnotationsFrom = null;
-  document.querySelectorAll('div.ytp-panel-menu div.ytp-menuitem[role=menuitemcheckbox]').forEach(d => {
-    if(d.innerText == 'Annotations' && d.getAttribute('aria-checked') == 'true') {
-      disabledAnnotationsFrom = d;
-      d.click();
-    }
-  });
+  document
+    .querySelectorAll(
+      "div.ytp-panel-menu div.ytp-menuitem[role=menuitemcheckbox]"
+    )
+    .forEach((d) => {
+      if (
+        d.innerText == "Annotations" &&
+        d.getAttribute("aria-checked") == "true"
+      ) {
+        disabledAnnotationsFrom = d;
+        d.click();
+      }
+    });
   // Blocking ADS
   if (result.isDebugModeOn) {
-    console.log('Hi from AdFreeYouTube, we are here to give you ad free experience on YouTube.');
+    console.log(
+      "Hi from AdFreeYouTube, we are here to give you ad free experience on YouTube."
+    );
     if (disabledAnnotationsFrom) {
-      console.log('Disabled annotations');
+      console.log("Disabled annotations");
       console.log(disabledAnnotationsFrom);
     }
     blockAdWithDebugging();
